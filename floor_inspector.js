be8c1ad8ae07eda7d280b0db90f771d751ee816b
ohataken <ohataken@outlook.com>
2017-06-25 19:31:03 +0900

    add floor_inspector.js

---
+++ floor_inspector.js
@@ -0,0 +1,28 @@
+module.exports = class FloorInspector {
+
+  constructor(floor) {
+    this.floor = floor;
+  }
+
+  unlessUndefined(obj, callback) {
+    if (obj) {
+      return callback(obj);
+    } else {
+      return undefined;
+    }
+  }
+
+  toString() {
+    var s = '';
+
+    s += `pc = ${this.floor.pc} / ${this.floor.insts.length}\n`;
+    s += `register = ${this.unlessUndefined(this.floor.register.value, (o) => { return o.value; })}\n`;
+    s += `instruction = ${this.unlessUndefined(this.floor.currentInstruction(), (o) => { return o.name })}\n`;
+    s += `inbox = ${this.floor.inbox.toString()}\n`;
+    s += `outbox = ${this.floor.outbox.toString()}\n`;
+    s += `expected = ${this.floor.expectedQueue.toString()}\n`;
+
+    return s;
+  }
+
+};
