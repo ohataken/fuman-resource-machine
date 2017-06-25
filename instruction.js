c769ad08ab11871ea28de1e398a25686924f6b71
ohataken <ohataken@outlook.com>
2017-06-26 00:16:01 +0900

    define Instruction#parse

--- instruction.js
+++ instruction.js
@@ -1,5 +1,19 @@
 module.exports = class Instruction {
 
+  static parse(string) {
+    const regexp = /(\w+)\s?(\d+)?;/g
+    const array = [];
+
+    while (regexp.lastIndex < string.length) {
+      const result = regexp.exec(string);
+      const name = result[1];
+      const operand = result[2] ? parseInt(result[2]) : undefined;
+      array.push(new this(name, [operand]));
+    }
+
+    return array;
+  }
+
   constructor(name, operands) {
     this.name = name;
     this.operands = operands || [];
